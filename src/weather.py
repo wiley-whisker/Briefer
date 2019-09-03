501841f5e22305080bcb4caaa99a4bb94e442b0c
wsm8855 <wsm8855@g.rit.edu>
2019-09-02 22:19:22 -0400

    Adding display for weather forecast. Added weather.py which handles getting and cleaning up the data from the API's. Also added simple display for current weather, but is still not functioning to full capacity.

---
+++ src/weather.py
@@ -0,0 +1,56 @@
+"""
+A module that handles acquiring all weather related content for the app.
+
+author: Wiley Matthews.
+date: 9/2/2019
+"""
+
+import requests
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from datetime import datetime
+
+AWC_BASE_URL = 'https://www.aviationweather.gov'
+AWC_PROG_PAGE = 'https://www.aviationweather.gov/progchart/sfc'
+DARKSKY_HOURLY = 'https://api.darksky.net/forecast/b3f609149b1cdc25c712bba1f8c20430/43.15,-77.68?exclude=minutely,daily,alerts,flags'
+
+
+def get_prog():
+    response = requests.get(AWC_PROG_PAGE)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    prog_img = soup.find('img', {"id": "image"})
+    prog_url = AWC_BASE_URL + prog_img['src']
+    return urlopen(prog_url).read()
+
+
+def get_forecast():
+    weather_data = requests.get(DARKSKY_HOURLY).json()
+
+    current_data = {
+                'time': datetime.fromtimestamp(weather_data['currently']['time']).strftime("%I:%M %p"),
+                'date': datetime.fromtimestamp(weather_data['currently']['time']).strftime("%m/%d"),
+                'summary': weather_data['currently']['summary'],
+                'icon': weather_data['currently']['icon'],
+                'precipProbability': weather_data['currently']['precipProbability'],
+                'temperature': weather_data['currently']['temperature']
+            }
+
+    gen_forecast = {
+        'summary': weather_data['hourly']['summary'],
+        'icon': weather_data['hourly']['icon']
+    }
+
+    hourly_forecast = []
+    for i in range(10):
+        hourly_forecast.append(
+            {
+                'time': datetime.fromtimestamp(weather_data['hourly']['data'][i]['time']).strftime("%m/%d - %I:%M %p"),
+                'date': datetime.fromtimestamp(weather_data['hourly']['data'][i]['time']).strftime("%m/%d"),
+                'summary': weather_data['hourly']['data'][i]['summary'],
+                'icon': weather_data['hourly']['data'][i]['icon'],
+                'precipProbability': weather_data['hourly']['data'][i]['precipProbability'],
+                'temperature': weather_data['hourly']['data'][i]['temperature']
+            }
+        )
+    return current_data, gen_forecast, hourly_forecast
--- src/weather_briefer.py
+++ src/weather_briefer.py
@@ -1,33 +1,19 @@
 """
-An app that displays the weather forcast for the day.
+An app that displays the weather forecast for the day.
 
 author: Wiley Matthews.
+date: 9/2/2019
 """
 
+# Standard libraries.
 import tkinter as tk
-import requests
 import io
 
+# External libraries.
 from PIL import Image, ImageTk
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
 
-AWC_BASE_URL = 'https://www.aviationweather.gov'
-AWC_PROG_PAGE = 'https://www.aviationweather.gov/progchart/sfc'
-DARKSKY_HOURLY_CALL = 'https://api.darksky.net/forecast/b3f609149b1cdc25c712bba1f8c20430/43.15,-77.68?exclude=minutely,daily,alerts,flags'
-
-
-def get_prog():
-    response = requests.get(AWC_PROG_PAGE)
-    data = response.text
-    soup = BeautifulSoup(data, features='html.parser')
-    prog_img = soup.find('img', {"id": "image"})
-    prog_url = AWC_BASE_URL + prog_img['src']
-    return urlopen(prog_url).read()
-
-
-def get_weather():
-    pass
+# Local libraries.
+from src import weather
 
 
 def resize_img(img, w, h):
@@ -42,24 +28,30 @@ def main():
     width = root.winfo_screenwidth()
     height = root.winfo_screenheight()
     root.geometry('%sx%s' % (width, height))
-    weather_frame = tk.Frame(root, background='#8f8f8f')
+    weather_frame = tk.Frame(root)
 
     weather_frame.pack(side='right', fill='both', expand=True)
 
     # Get and display prog chart
-    prog_img = resize_img(get_prog(), width, height)
+    prog_img = resize_img(weather.get_prog(), width, height)
     prog_chart = ImageTk.PhotoImage(prog_img)
     canvas = tk.Canvas(root, bg='white', width=width//2, height=height)
     canvas.pack(anchor='w')
     canvas.create_image(width//2, 0, image=prog_chart, anchor='ne')
 
-    #
+    # Get and display weather data
     tk.Label(weather_frame, text="Today's Weather", font=("Helvetica", 40)).pack()
-
+    current, outlook, forecast = weather.get_forecast()
+    tk.Label(weather_frame, text=current['summary'], font=("Helvetica", 20)).pack()
+    sym = r'C:\Users\Wiley\Documents\python_projects\Briefer\assets\cloudy.png'  # static path for now.
+    sym_image = Image.open(sym)
+    photo = ImageTk.PhotoImage(sym_image)
+    cv = tk.Canvas(weather_frame, bg='#d1d1d1', width=photo.width(), height=photo.height())
+    cv.pack()
+    cv.create_image(0, 0, image=photo, anchor='nw')
+    tk.Label(weather_frame, text=current['date'] + " - " + current['time'], font=("Helvetica", 20)).pack()
 
     root.mainloop()
-
-    # Get images
 
 if __name__ == '__main__':
     main()
